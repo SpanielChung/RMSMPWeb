e683678ed1e6cd1b539ca97544c84216d355050c
Dan Young <dan.young12@gmail.com>
2017-08-27 16:04:45 -0400

    updated model

---
+++ RMSMPWeb/Models/DataPoints.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMSMPWeb
+{
+    public class DataPoints
+    {
+        public DataPoints()
+        {
+
+        }
+        public DateTime timeStamp { get; set; }
+        public DateTime groupingStamp { get; set; }
+        public string deviceId { get; set; }
+        public double returnAirTemp { get; set; }
+        public double returnAirHumidity { get; set; }
+        public double dischargeAirTemp { get; set; }
+        public double dischargeAirHumidity { get; set; }
+        public double suctionTemp { get; set; }
+        public double compressionTemp { get; set; }
+        public double condensorTemp { get; set; }
+        public double evaporatorTemp { get; set; }
+        public double fanCurrent { get; set; }
+        public double compressorCurrent { get; set; }
+
+        public int sourceCount { get; set; }
+
+
+    }
+}
